819b27b70f2dc60b07ed6dd895c166ff6ebac502
Amr Mustafa <amrmostafa1311@gmail.com>
2023-04-16 23:52:43 +0200

    higher order functions added

---
+++ higher-order-functions/app.js
@@ -0,0 +1,57 @@
+// foreach
+
+// const arr = [1, 2, 3, 4, 5];
+
+// arr.forEach((item) => {
+//     console.log(item);
+// }))
+
+// arr.forEach(item => console.log(item));
+
+// map
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const newArr = arr.map((item) => {
+//     return item * 2;
+// }
+
+// const newArr = arr.map(item => item * 2);
+
+// console.log(newArr);
+
+// filter
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const newArr = arr.filter((item) => {
+//     return item % 2 === 0;
+// }
+
+// const newArr = arr.filter(item => item % 2 === 0);
+
+// console.log(newArr);
+
+// sort
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const newArr = arr.sort((a, b) => {
+//     return a - b;
+// }
+
+// const newArr = arr.sort((a, b) => a - b);
+
+// console.log(newArr);
+
+// reduce
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const sum = arr.reduce((total, next) => {
+//     return total + next;
+// }
+
+// const sum = arr.reduce((total, next) => total + next);
+
+// console.log(sum);
